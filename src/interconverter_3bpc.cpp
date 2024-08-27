9fd78039e8cfa52aa84f3067e142e5671ff6352f
Fufu Fang <fangfufu@fangfufu.co.uk>
2024-08-27 01:51:38 +0100

    tidying up interconverter_3bpc

--- src/interconverter_3bpc.cpp
+++ src/interconverter_3bpc.cpp
@@ -1,14 +1,16 @@
 #include "interconverter_3bpc.hpp"
 
 #define BRIGHTNESS_BIAS     0b00001000
-#define VALID_BIT_MASK          0b11100000
-#define IGNORED_BIT_COUNT   5
+#define VALID_BIT_MASK      0b11100000
 
 void byte_to_pixel_3bpc(uint8_t byte, cv::Vec3b& pixel) 
 {
     for (int i = 0; i < 3; i++) {
-        // Note that we want to bias the colour values by 8
-        pixel[i] = (byte << (3 * i) & 0b11100000) | BRIGHTNESS_BIAS;
+        /* 
+         * Note that we make the channel slightly brighter, so there is no
+         * ambiguity that the channel brightness value is below the threshold
+         */
+        pixel[i] = (byte << (3 * i) & VALID_BIT_MASK) | BRIGHTNESS_BIAS;
     }
 }
 

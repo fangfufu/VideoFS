a99372a77275e867d860eea6c7a993e57fd6a25d
Fufu Fang <fangfufu2003@gmail.com>
2024-09-13 22:50:14 +0100

    removed trailing some whitespaces

--- src/FrameEncoder/FrameEncoder3BPC.cpp
+++ src/FrameEncoder/FrameEncoder3BPC.cpp
@@ -6,11 +6,11 @@
 
 
 FrameEncoder3BPC::FrameEncoder3BPC(const std::string& filename,
-                    int video_width, 
-                    int video_height, 
-                    int fps, 
+                    int video_width,
+                    int video_height,
+                    int fps,
                     int macroblock_size,
-                    int frame_repeat) : 
+                    int frame_repeat) :
                     filename(filename),
                     video_height(video_height),
                     video_width(video_width),
@@ -20,10 +20,10 @@ FrameEncoder3BPC::FrameEncoder3BPC(const std::string& filename,
                     frame_height(video_height/macroblock_size),
                     frame_width(video_width/macroblock_size),
                     frame_size(frame_height * frame_width) {
-    if(!video_writer.open(filename, 
-        cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 
-        fps, 
-        cv::Size(frame_width, frame_height), 
+    if(!video_writer.open(filename,
+        cv::VideoWriter::fourcc('M', 'J', 'P', 'G'),
+        fps,
+        cv::Size(frame_width, frame_height),
         true)) {
             throw std::runtime_error(
                 "Error: Could not open the video file for writing.");
